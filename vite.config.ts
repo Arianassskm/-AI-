a9f5f91ceaedc9834d49af95e824807175d96fe9
ek0kies <asu.jojo@gmail.com>
2025-01-03 18:14:23 +0800

    [web] update 解决跨域

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,23 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
+  },
+  server: {
+    proxy: {
+      "/baidu-api": {
+        target: "https://aip.baidubce.com",
+        changeOrigin: true,
+        ws: true,
+        rewrite: (path) => path.replace(new RegExp(`^/baidu-api`), ""),
+      },
+    },
+    warmup: {
+      clientFiles: ["./index.html", "./src/{views,components}/*"],
+    },
   },
 });
